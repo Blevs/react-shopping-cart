d3b1230bca5e83d18738c3fbaf02a4ff188a9e85
Henry Blevins <heblevi@gmail.com>
2019-08-13 14:56:52 -0400

    Completed recitation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
@@ -7,36 +7,42 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Contexts
+import ProductContext from './contexts/ProductContext.js';
+import CartContext from './contexts/CartContext.js';
+
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+   
+  const [cart, setCart] = useState(localStorage.getItem('cart')
+                                   ? JSON.parse(localStorage.getItem('cart'))
+                                   : []);
 
-	const addItem = item => {
-		setCart([...cart, item]);
-	};
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart]);
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
+  const addItem = item => {
+    setCart([...cart, item]);
+  };
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+  const removeItem = id => {
+    setCart(cart.filter(item => item.id !== id));
+  };
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
-	);
+  return (
+    <ProductContext.Provider value={{ products, addItem }} >
+      <CartContext.Provider value={{ cart, removeItem }}>
+        <div className="App">
+          <Navigation />
+
+          {/* Routes */}
+          <Route exact path="/" component={Products} />
+          <Route path="/cart" component={ShoppingCart} />
+        </div>
+      </CartContext.Provider>
+    </ProductContext.Provider>
+  );
 }
 
 export default App;
